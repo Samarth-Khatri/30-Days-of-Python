ce8535d606ec051a8ad3f830b2b09977e414abd0
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-01-30 22:10:58 +0530

    Updated list

---
+++ Basics/list.py
@@ -0,0 +1,19 @@
+abc = "hello there"
+
+print(abc)
+
+list_var= ["some string", "hi"]  #Defining list
+
+print(list_var)
+
+list_var.append("another item")  #Adding item to list_var
+
+print(list_var)
+
+list_var.pop()  #Deleting item form list_var
+
+print(list_var)
+
+print(len(list_var))  #Checking length of list
+
+print(len("Hello my name is Samarth"))
