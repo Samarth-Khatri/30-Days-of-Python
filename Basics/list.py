fa3a5441f9f7b233e6695b537c60e632458500c2
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-01-31 10:33:25 +0530

    Updated list

--- Basics/list.py
+++ Basics/list.py
@@ -17,3 +17,15 @@ print(list_var)
 print(len(list_var))  #Checking length of list
 
 print(len("Hello my name is Samarth"))
+
+print(list_var[0]) #Displaying through string
+print(list_var[1])
+
+sec_list = ["A", "B", "C"]
+ran_list = sec_list[2]  #Assigning list
+print(ran_list)
+
+list2 = ["Hello", "Hi", "Bye"]
+list2.pop(0)
+list2.pop(1)
+print(list2)
