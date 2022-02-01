26e65451529bb56ac79cfca8ea348cf89559199b
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-02-01 10:43:20 +0530

    learned about dictionaries

---
+++ Basics/dictionaries.py
@@ -0,0 +1,16 @@
+#Dictionaries and Tuples
+
+a_dict = {"abc": "A string"}
+print(a_dict)
+
+a_dict = {"abc": "A string", "xyz": "Another sring"}
+print(a_dict)
+
+a_dict["abc"] = "A new string"
+print(a_dict)
+
+print(a_dict["xyz"])
+
+abc = {0: "first"}
+abc[1] = "second"
+print(abc)
