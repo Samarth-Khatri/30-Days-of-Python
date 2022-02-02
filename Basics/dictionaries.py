b614aa9a61da858ed69b035ce98e3fe80a8fc20f
Samarth Khatri <samarth.khatri0000@gmail.com>
2022-02-02 11:17:07 +0530

    learned about dictionaries and tuples

--- Basics/dictionaries.py
+++ Basics/dictionaries.py
@@ -14,3 +14,18 @@ print(a_dict["xyz"])
 abc = {0: "first"}
 abc[1] = "second"
 print(abc)
+
+
+tup = ()
+tup = ("xyz", "xyz")
+
+print(tup)
+
+new_tup = (("abc xyz", "123") , ("hello"))
+print(new_tup)
+print(new_tup[0])
+print(new_tup[0][0])
+print(new_tup[0][0][0])
+
+new_tup += (("another", "456"),) #Adiing new tupple
+print(new_tup)
